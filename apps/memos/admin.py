5498e5d07c26ef92bc5108e8e2deef240768bfaa
risingpark <risingpark@gitauto.com>
2025-10-17 16:03:18 +0900

    feat: Phase 7 - 관리자 설정 완료
    - Memo 모델 관리자 등록
      - list_display, list_filter, search_fields 설정
      - readonly_fields 설정 (created_at, updated_at)
      - date_hierarchy 설정
      - fieldsets 구성
    - User 모델 관리자 등록
      - BaseUserAdmin 상속
      - 추가 필드 표시 (created_at, updated_at)
      - 검색 및 필터 기능 설정
    
    Resolves #7

--- apps/memos/admin.py
+++ apps/memos/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Memo
 
-# Register your models here.
+
+@admin.register(Memo)
+class MemoAdmin(admin.ModelAdmin):
+    """
+    메모 관리자 설정
+    """
+    list_display = ["title", "user", "created_at", "updated_at"]
+    list_filter = ["created_at", "updated_at", "user"]
+    search_fields = ["title", "content", "user__username"]
+    readonly_fields = ["created_at", "updated_at"]
+    date_hierarchy = "created_at"
+    
+    fieldsets = (
+        ("메모 정보", {
+            "fields": ("user", "title", "content")
+        }),
+        ("날짜 정보", {
+            "fields": ("created_at", "updated_at"),
+            "classes": ("collapse",)
+        }),
+    )
--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    """
+    사용자 관리자 설정
+    """
+    list_display = ["username", "email", "first_name", "last_name", "is_staff", "date_joined"]
+    list_filter = ["is_staff", "is_superuser", "is_active", "date_joined"]
+    search_fields = ["username", "email", "first_name", "last_name"]
+    
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ("추가 정보", {
+            "fields": ("created_at", "updated_at")
+        }),
+    )
+    
+    readonly_fields = ["date_joined", "last_login", "created_at", "updated_at"]
